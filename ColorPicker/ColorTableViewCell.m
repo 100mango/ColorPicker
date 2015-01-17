65d265dc458d571bfa37cac46f1c441ef9a4eb9d
100mango <100mango@users.noreply.github.com>
2015-01-17 16:01:50 +0800

    add color tableview cell

---
+++ ColorPicker/ColorTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ColorTableViewCell.m
+//  ColorPicker
+//
+//  Created by Mango on 15/1/17.
+//  Copyright (c) 2015年 Mango. All rights reserved.
+//
+
+#import "ColorTableViewCell.h"
+
+@implementation ColorTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
